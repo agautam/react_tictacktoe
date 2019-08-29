5826e4d6870ebba1ab1158440a4a35df70558ba8
Arvind Gautam <1295124+agautam@users.noreply.github.com>
2019-08-29 12:15:46 -0400

    algorithmic update to rendering board without writing each square in code

--- src/index.js
+++ src/index.js
@@ -15,24 +15,21 @@ class Board extends React.Component {
         return <Square value={this.props.board[i]} onClick={() => this.props.onClick(i)} />;
     }
 
+    renderRow(row) {
+        const squares = row.map(square => this.renderSquare(square));
+        const div = <div className="board-row">
+            {squares}
+        </div>
+
+        return div;
+    }
+
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.renderRow([0, 1, 2])}
+                {this.renderRow([3, 4, 5])}
+                {this.renderRow([6, 7, 8])}
             </div>
         );
     }
